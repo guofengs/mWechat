23e9a48c8176cf356ad173198172678a2e19408b
guofeng <940385517@qq.com>
2018-03-01 16:51:31 +0800

    1

--- build/vue-loader.config.js
+++ build/vue-loader.config.js
@@ -3,3 +3,6 @@
  * @date   2018/3/1 下午1:40
  * @Description: vue-loader的配置
  */
+
+ module.exports = {
+}
--- build/webpack.server.config.js
+++ build/webpack.server.config.js
@@ -3,3 +3,30 @@
  * @date   2018/3/1 下午1:42
  * @Description: webpack 服务端配置
  */
+
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const base = require('./webpack.base.config')
+const VueSSRPlugin = require('vue-ssr-webpack-plugin')
+
+module.exports = merge(base, {
+  target: 'node',
+  entry: './src/server-entry.js',
+  output: {
+    filename: 'server-bundle.js',
+    libraryTarget: 'commonjs2'
+  },
+  resolve: {
+    alias: {
+      'create-api': './create-api-server.js'
+    }
+  },
+  externals: Object.keys(require('../package.json').dependencies),
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+      'process.env.VUE_ENV': '"server"'
+    }),
+    new VueSSRPlugin()
+  ]
+})
