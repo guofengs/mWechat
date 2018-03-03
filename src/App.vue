71534039146d49009224df47a54db5d842327eb9
guofeng <iquanku@163.com>
2018-03-03 15:20:00 +0800

    提交舍和

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,10 @@
   <div id="app">
      <img src="./assets/logo.png">
      <ul>
-       <li><router-link to="/" exact>Home</router-link></li>
-       <li><router-link to="/topics">Topics</router-link></li>
+       <li><router-link to="/" exact>主页</router-link></li>
+       <li><router-link to="/topics">话题</router-link></li>
        <li><router-link to="/counter">Counter</router-link></li>
-       <li><router-link to="/about">About</router-link></li>
+       <li><router-link to="/about">关于</router-link></li>
      </ul>
      <router-view class="view"></router-view>
   </div>
